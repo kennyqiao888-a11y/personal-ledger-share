2c100b6073fbe93e8f85c4b2ee83052d01600772
ZyanZealous <zyanzealous@gmail.com>
2026-06-04 00:05:43 -0400

    Auto refresh ledger share 2026-06-04 00:05:43

--- ledger-data.js
+++ ledger-data.js
@@ -2680,6 +2680,16 @@ window.__LEDGER_DATA__ = {
       category: "生意收入",
       account: "现金",
       note: "卖 5060 主机"
+    },
+    {
+      id: "20260604-4",
+      type: "expense",
+      amount: 200.00,
+      date: "2026-06-04",
+      time: "00:02",
+      category: "提成",
+      account: "现金",
+      note: "提成"
     }
   ]
 };
