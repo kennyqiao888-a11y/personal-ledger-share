8c708d0872d880bcce15ef642d1a0b64eb76f2fd
ZyanZealous <zyanzealous@gmail.com>
2026-06-11 13:30:33 -0400

    Auto refresh ledger share 2026-06-11 13:30:33

--- ledger-data.js
+++ ledger-data.js
@@ -1,5 +1,5 @@
 window.__LEDGER_DATA__ = {
-  updatedAt: "2026-06-11 12:24 EDT",
+  updatedAt: "2026-06-11 13:29 EDT",
   entries: [
     {
       id: "20260313-1",
@@ -2920,6 +2920,16 @@ window.__LEDGER_DATA__ = {
       category: "采购",
       account: "银行卡",
       note: "Newegg 采购"
+    },
+    {
+      id: "20260611-6",
+      type: "income",
+      amount: 850.00,
+      date: "2026-06-11",
+      time: "13:29",
+      category: "生意收入",
+      account: "现金",
+      note: "卖 Legion 3060 笔记本"
     }
   ]
 };
