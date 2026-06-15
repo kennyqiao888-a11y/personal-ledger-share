8c2e00ff1523dc3b1b5ba725d420f2e42b9192a0
ZyanZealous <zyanzealous@gmail.com>
2026-06-15 16:55:13 -0400

    Auto refresh ledger share 2026-06-15 16:55:13

--- ledger-data.js
+++ ledger-data.js
@@ -1,5 +1,5 @@
 window.__LEDGER_DATA__ = {
-  updatedAt: "2026-06-11 15:58 EDT",
+  updatedAt: "2026-06-15 16:53 EDT",
   entries: [
     {
       id: "20260313-1",
@@ -2950,6 +2950,16 @@ window.__LEDGER_DATA__ = {
       category: "生意收入",
       account: "现金",
       note: "卖 Legion 3060 笔记本"
+    },
+    {
+      id: "20260615-1",
+      type: "expense",
+      amount: 145.00,
+      date: "2026-06-15",
+      time: "16:53",
+      category: "吃饭",
+      account: "现金",
+      note: "吃饭"
     }
   ]
 };
